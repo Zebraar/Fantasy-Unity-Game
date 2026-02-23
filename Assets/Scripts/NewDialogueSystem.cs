ae68b25ba3c045538c3f9d61b1ed1d77ffb0c6f3
Zebrar <777evgeniy2301@gmail.com>
2026-02-23 15:23:26 +0500

    fix: Исправил баг, из-за которого не показывалась реплика после выбора

--- Assets/Scripts/NewDialogueSystem.cs
+++ Assets/Scripts/NewDialogueSystem.cs
@@ -58,6 +58,7 @@ public class NewDialogueSystem : MonoBehaviour
     {
         string playerChoice = choiceButtons[buttonNum].gameObject.GetComponentInChildren<Text>().text;
         dialogueLogic.NextChoice(playerChoice);
+        UpdateUI();
     }
 
     public void EndDialogue()
