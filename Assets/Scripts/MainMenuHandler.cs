136714ac117466379609ca41302369f3ff9fbc52
Zebrar <777evgeniy2301@gmail.com>
2026-03-05 12:02:12 +0500

    feat: Добавил логику для главного меню и чуть-чуть улучшил его дизайн

--- Assets/Scripts/MainMenuHandler.cs
+++ Assets/Scripts/MainMenuHandler.cs
@@ -1,8 +1,40 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuHandler : MonoBehaviour
 {
+
+    public GameObject settingsPanel;
+    public GameObject gameInfoPanel;
     
-    
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void SettingsMenuShow()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void SettingsMenuHide()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    public void GameInfoShow()
+    {
+        gameInfoPanel.SetActive(true);
+    }
+
+    public void GameInfoHide()
+    {
+        gameInfoPanel.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
 
 }
